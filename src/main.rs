9eccde7760cad4c9d67dd5ce919485eabab59f53
w4daka <213398204+w4daka@users.noreply.github.com>
2026-08-23 15:12:49 +0900

    feat 最大値を求めるby関数+Option

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,29 @@ fn main() {
         .map(|x| x.parse::<i32>().unwrap())
         .collect();
 
-    let mut sum = 0;
-    let count = n.len() as i32;
-    for element in n {
-        sum += element;
+    match search_max(n) {
+        None => println!("最大値なし"),
+        Some(value) => println!("{}", value),
     }
-
-    println!("{}", sum / count)
 }
 
 fn input() -> String {
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
     input
+}
+
+fn search_max(numbers: Vec<i32>) -> Option<i32> {
+    let mut max = None;
+    for element in numbers {
+        match max {
+            None => max = Some(element),
+            Some(value) => {
+                if value < element {
+                    max = Some(element)
+                }
+            }
+        }
+    }
+    max
 }
