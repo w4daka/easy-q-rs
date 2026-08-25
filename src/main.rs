6dff88e6b5eac2db051ac9e60e4c679ad06f542b
w4daka <213398204+w4daka@users.noreply.github.com>
2026-08-25 16:14:00 +0900

    feat 文字列表示の方法を整理

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,45 @@
-use std::io::stdin;
+// use std::io::stdin;
 fn main() {
-    let input = input();
+    // let input = input();
+    //
+    // let n: Vec<i32> = input
+    //     .split_whitespace()
+    //     .map(|x| x.parse::<i32>().unwrap())
+    //     .collect();
 
-    let n: Vec<i32> = input
-        .split_whitespace()
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect();
-
-    let a = &n[0];
-    let b = &n[1];
-
-    println!("{}", max_ref(a, b))
+    let s = String::from("hello");
+    take_string2(&s);
 }
 
-fn input() -> String {
-    let mut input = String::new();
-    stdin().read_line(&mut input).unwrap();
-    input
+// fn input() -> String {
+//     let mut input = String::new();
+//     stdin().read_line(&mut input).unwrap();
+//     input
+// }
+// Stringを所有して渡す->move->元の変数は使えない
+// Stringを借りて渡すー>borrowー>元の変数を使える
+// 独立したStringが必要 -> clone ->別のStringを作る
+fn take_string1(s: String) -> String {
+    s
 }
 
-fn max_ref(a: &i32, b: &i32) -> i32 {
-    let mut ans = *a;
-    if a < b {
-        ans = *b;
-    }
-    ans
+fn take_string2(s: &str) {
+    println!("{}", s)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_max_ref() {
-        assert_eq!(max_ref(&10, &20), 20);
-        assert_eq!(max_ref(&30, &5), 30);
-        assert_eq!(max_ref(&7, &7), 7);
-        assert_eq!(max_ref(&-10, &-20), -10);
-    }
+fn take_string3(s: &String) -> String {
+    s.clone()
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test_max_ref() {
+//         assert_eq!(max_ref(&10, &20), 20);
+//         assert_eq!(max_ref(&30, &5), 30);
+//         assert_eq!(max_ref(&7, &7), 7);
+//         assert_eq!(max_ref(&-10, &-20), -10);
+//     }
+// }
